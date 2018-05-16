9d0c42c1620c02730c948c8b543fa030186da4d5
beckybair <beckyhowen@gmail.com>
2018-05-16 17:10:13 -0500

    updated shuffle but getting error - commited for review

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
+const restart = document.querySelector('.restart');
 let list = document.querySelectorAll('.card');
 let cardList = Array.prototype.slice.call(document.querySelectorAll('.card'));
 
@@ -10,11 +11,17 @@ let cardList = Array.prototype.slice.call(document.querySelectorAll('.card'));
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-cardList = shuffle(cardList);
-for (var i = 0; i < list.length; i++) {
-  list[i].outerHTML = cardList[i].outerHTML;
+function reset() {
+  cardList = shuffle(cardList);
+  for (var i = 0; i < list.length; i++) {
+    list[i].outerHTML = cardList[i].outerHTML;
+  }
+  console.log(list);
+  
 }
-console.log(list);
+
+restart.addEventListener('click', reset);
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -35,23 +42,31 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
+
+ // Flip Card
  *  - display the card's symbol (put this functionality in another function that you call from this one)
+
+ // Add to "openCards" list
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- 
- * ========================
- 
- * Use shuffle to random the cards
- * Setup Modal and display:
- *   - Congrats message
- *   - Play again?
- *   - Time it took
- *   - Star rating
- *   - Number of moves
+        * Setup Modal and display:
+        *   - Congrats message
+        *   - Play again?
+        *   - Time it took
+        *   - Star rating
+        *   - Number of moves
+ */
+
+
+
+
+
+ /*
+ * DONE - Use shuffle to random the cards
  * Mobile first - then tablet - then desktop
  * Update Readme.md
  * Restart button:
